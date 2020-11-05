4bad65f11190e82a0ec562702a82b96fa54f77bd
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-11-04 22:44:16 -0600

    feat: added event listener to projects tab

--- js/personal_website_js.js
+++ js/personal_website_js.js
@@ -1,5 +1,8 @@
 var abtMeListener = function (event){
-
 }
 
-abtMeListener().getElementById('abtMe').addEventListener('click', event);
+abtMeListener().getElementById('abtMe').addEventListener('click', show);
+
+var projectListener = function(event) {
+}
+projectListener().getElementById("projects").addEventListener('click', show);
